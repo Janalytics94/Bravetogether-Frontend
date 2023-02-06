b5d8e6b2b5b9820378a439fc4605852d5ab7c717
Philipp Page <github@philipp-page.de>
2023-02-06 15:06:26 +0100

    Remove unncessary Suspense.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import React, { Suspense } from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 import "./i18next";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Suspense fallback={<h1>Loading profile...</h1>}>
-      <App />
-    </Suspense>
+    <App />
   </React.StrictMode>
 );
 
