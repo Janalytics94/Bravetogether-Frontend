e0cc1b362e2cd972f10fb9df2a51e536821b5a0f
Philipp Page <github@philipp-page.de>
2023-03-02 09:15:56 +0000

    Use layout effect for scroll listener in BE BRAVE circle.

--- src/components/BeBoldCircle/BeBoldCircle.js
+++ src/components/BeBoldCircle/BeBoldCircle.js
@@ -1,26 +1,24 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useLayoutEffect, useState } from "react";
 import "./BeBoldCircle.css";
 
 const BeBoldCircle = () => {
   const [position, setPosition] = useState(0);
   const [opacatiyCircle, setOpacatiyCircle] = useState(1);
 
-  const changedCircle = () => {
-    setPosition(window.scrollY);
-  };
+  useLayoutEffect(() => {
+    window.addEventListener("scroll", () => setPosition(window.scrollY));
+  }, []);
 
   useEffect(() => {
     setOpacatiyCircle(1 - position / 100);
   }, [position]);
-
-  window.addEventListener("scroll", changedCircle);
 
   return (
     <>
       {position < 100 ? (
         <div className="background_circle" style={{ opacity: opacatiyCircle }}>
           <div className="circle">
-            <h1 className="text"> BE BRAVE </h1>
+            <h1 className="text">BE BRAVE</h1>
           </div>
         </div>
       ) : (
